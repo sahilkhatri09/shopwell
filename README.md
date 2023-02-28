f3e08283cf2857d393f5e1793361144eb186e8dd
sahil <sahilkhatri1170@gmail.com>
2023-02-28 17:13:39 +0530

    intial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# MERN E-COMMERCE TUTORIAL
+
+Hi! My name is **Sahil Khatri**
+
+Ecommerce Site using tech stack MongoDb Expressjs Reactjs Nodejs
---
+++ frontend/src/component/layout/About/About.js
@@ -0,0 +1,46 @@
+import React from "react";
+import "./aboutSection.css";
+import { Button, Typography, Avatar } from "@material-ui/core";
+import YouTubeIcon from "@material-ui/icons/YouTube";
+import InstagramIcon from "@material-ui/icons/Instagram";
+const About = () => {
+  const visitInstagram = () => {
+    window.location = "https://instagram.com/_gaurav.thakkar_";
+  };
+  return (
+    <div className="aboutSection">
+      <div></div>
+      <div className="aboutSectionGradient"></div>
+      <div className="aboutSectionContainer">
+        <Typography component="h1">About Us</Typography>
+
+        <div>
+          <div>
+            <Avatar
+              style={{ width: "10vmax", height: "10vmax", margin: "2vmax 0" }}
+              src="https://res.cloudinary.com/dsyfkppjl/image/upload/v1675696776/software-developer-6521720__340_prauxy.jpg"
+              alt="Founder"
+            />
+            <Typography>Gaurav</Typography>
+            <Button onClick={visitInstagram} color="primary">
+              Visit Instagram
+            </Button>
+            <span>
+              This is a sample wesbite made by @Gaurav. 
+            </span>
+          </div>
+          <div className="aboutSectionContainer2">
+            <Typography component="h2">Our Brands</Typography>
+            
+
+            <a href="https://instagram.com/_gaurav.thakkar_" target="blank">
+              <InstagramIcon className="instagramSvgIcon" />
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
